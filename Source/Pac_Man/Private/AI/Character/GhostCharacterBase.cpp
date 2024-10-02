6fd108a05602d7705f2d1c6c11f721c877f0ab06
Pepen <ti-max__1998@hotmail.com>
2024-10-01 21:44:21 -0400

    Update GhostCharacterBase.cpp
    Fix :add body function in AGhostCharacterBase

--- Source/Pac_Man/Private/AI/Character/GhostCharacterBase.cpp
+++ Source/Pac_Man/Private/AI/Character/GhostCharacterBase.cpp
@@ -21,12 +21,30 @@ void AGhostCharacterBase::BeginPlay()
 	Super::BeginPlay();
 }
 
+void AGhostCharacterBase::SetFearMode()
+{
+	CurrentState = EGhostState::Fear;
+	SetFearMode_BP();
+}
+
+void AGhostCharacterBase::SetDeadMode()
+{
+	CurrentState = EGhostState::Dead;
+	SetDeadMode_BP();
+}
+
+void AGhostCharacterBase::SetAliveMode()
+{
+	CurrentState = EGhostState::Alive;
+	SetAliveMode_BP();
+}
+
 void AGhostCharacterBase::OnHitSomething(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
                                          const FHitResult& SweepResult)
 {
 	if (CurrentState == EGhostState::Fear)return;
-	
+
 	if (const APac_ManCharacter* PacManEntity = Cast<APac_ManCharacter>(OtherActor))
 	{
 		PacManEntity->GetDataComponent()->RemoveLife();
